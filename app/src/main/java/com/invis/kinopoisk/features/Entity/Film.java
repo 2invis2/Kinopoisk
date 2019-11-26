6373b37b13fa6721cb79b42b5779fc3bd045f76d
Oleg <invisible6462@gmail.com>
2019-11-26 14:53:27 +0700

    Fragment
    Add a description fragment

--- app/src/main/java/com/invis/kinopoisk/features/Entity/Film.java
+++ app/src/main/java/com/invis/kinopoisk/features/Entity/Film.java
@@ -1,5 +1,8 @@
 package com.invis.kinopoisk.features.Entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -9,7 +12,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-public class Film {
+public class Film implements Parcelable {
     private int id;
     private String localized_name;
     private String name;
@@ -18,4 +21,44 @@ public class Film {
     private String image_url;
     private String description;
     private List<String> genres;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.localized_name);
+        dest.writeString(this.name);
+        dest.writeInt(this.year);
+        dest.writeDouble(this.rating);
+        dest.writeString(this.image_url);
+        dest.writeString(this.description);
+        dest.writeStringList(this.genres);
+    }
+
+    protected Film(Parcel in) {
+        this.id = in.readInt();
+        this.localized_name = in.readString();
+        this.name = in.readString();
+        this.year = in.readInt();
+        this.rating = in.readDouble();
+        this.image_url = in.readString();
+        this.description = in.readString();
+        this.genres = in.createStringArrayList();
+    }
+
+    public static final Parcelable.Creator<Film> CREATOR = new Parcelable.Creator<Film>() {
+        @Override
+        public Film createFromParcel(Parcel source) {
+            return new Film(source);
+        }
+
+        @Override
+        public Film[] newArray(int size) {
+            return new Film[size];
+        }
+    };
 }
--- app/src/main/java/com/invis/kinopoisk/features/presenter/KinopoiskView.java
+++ app/src/main/java/com/invis/kinopoisk/features/presenter/KinopoiskView.java
@@ -1,4 +1,7 @@
 package com.invis.kinopoisk.features.presenter;
 
+import com.invis.kinopoisk.features.Entity.Film;
+
 public interface KinopoiskView extends MvpView {
+    void onFilmSelect(Film film);
 }
